27655cfb3f6416bb4039e211664c652b5905d11f
FlorianWoelki <florianwoelki@gmx.de>
2023-10-31 13:46:02 +0100

    fix(plugin): disable illuminate for netrw

--- after/plugin/illuminate.lua
+++ after/plugin/illuminate.lua
@@ -6,6 +6,12 @@ require('illuminate').configure({
   },
   delay = 100,
   min_count_to_highlight = 1,
+  filetypes_denylist = {
+    'dirbuf',
+    'dirvish',
+    'fugitive',
+    'netrw',
+  },
 })
 
 vim.cmd('hi IlluminatedWordRead gui=none guibg=#3B3B3B')
