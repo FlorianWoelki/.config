537259409702d7e7bcbf63852f446d82d11f2d4f
FlorianWoelki <florianwoelki@gmx.de>
2023-11-03 15:49:22 +0100

    feat(lsp): add formatting mapping and remove format on save

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -13,7 +13,7 @@ lsp.ensure_installed({
 lsp.nvim_workspace()
 
 local cmp = require('cmp')
-local cmp_select = {behavior = cmp.SelectBehavior.Select}
+local cmp_select = { behavior = cmp.SelectBehavior.Select }
 local cmp_mappings = lsp.defaults.cmp_mappings({
   ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
   ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
@@ -29,31 +29,33 @@ lsp.setup_nvim_cmp({
 })
 
 lsp.set_preferences({
-    suggest_lsp_servers = false,
-    sign_icons = {
-        error = 'E',
-        warn = 'W',
-        hint = 'H',
-        info = 'I'
-    }
+  suggest_lsp_servers = false,
+  sign_icons = {
+    error = 'E',
+    warn = 'W',
+    hint = 'H',
+    info = 'I'
+  }
 })
 
 lsp.on_attach(function(client, bufnr)
-  local opts = {buffer = bufnr, remap = false}
+  local opts = { buffer = bufnr, remap = false }
 
   local function on_list(options)
     vim.fn.setqflist({}, " ", options)
     vim.api.nvim_command('cfirst')
   end
 
-  vim.keymap.set("n", "gd", function() vim.lsp.buf.definition({
-    on_list = on_list,
-  }) end, opts)
+  vim.keymap.set("n", "gd", function()
+    vim.lsp.buf.definition({
+      on_list = on_list,
+    })
+  end, opts)
   vim.keymap.set("n", "gD", function()
     vim.cmd([[
     vsplit
     ]])
-    vim.lsp.buf.definition({on_list = on_list})
+    vim.lsp.buf.definition({ on_list = on_list })
   end, opts)
   vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
   vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
@@ -64,16 +66,14 @@ lsp.on_attach(function(client, bufnr)
   vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
   vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
   vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+  vim.keymap.set("n", "gq", function()
+    vim.lsp.buf.format({ async = false, timeout_ms = 10000 })
+  end, opts)
 end)
 
 lsp.setup()
 
 vim.diagnostic.config({
-    virtual_text = true,
-    signs = false
-})
-
-vim.api.nvim_create_autocmd("BufWritePre", {
-  pattern = { "*.tsx,*.ts,*.jsx,*.js" },
-  command = "EslintFixAll"
+  virtual_text = true,
+  signs = false
 })
