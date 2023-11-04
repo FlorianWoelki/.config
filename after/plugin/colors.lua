5e8dc24557f14f4df1e5c2cd375ad6558053cc9f
FlorianWoelki <FlorianWoelki@gmx.de>
2023-11-04 15:40:08 +0100

    refactor: adjust colors for illuminate and general palette

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -8,4 +8,3 @@ function ColorMe(color)
 end
 
 ColorMe()
-
--- after/plugin/illuminate.lua
+++ after/plugin/illuminate.lua
@@ -1,18 +1,17 @@
-require('illuminate').configure({
-  providers = {
-    'lsp',
-    'treesitter',
-    'regex'
-  },
-  delay = 100,
-  min_count_to_highlight = 1,
-  filetypes_denylist = {
-    'dirbuf',
-    'dirvish',
-    'fugitive',
-    'netrw',
-  },
+require("illuminate").configure({
+	providers = {
+		"lsp",
+		"treesitter",
+		"regex",
+	},
+	delay = 100,
+	min_count_to_highlight = 2,
+	filetypes_denylist = {
+		"dirbuf",
+		"dirvish",
+		"fugitive",
+		"netrw",
+	},
 })
 
-vim.cmd('hi IlluminatedWordRead gui=none guibg=#3B3B3B')
-
+vim.cmd("hi IlluminatedWordRead gui=none guibg=#444444")
