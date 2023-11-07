d792b6aa031109e4e81cd96488632c1e686219fa
FlorianWoelki <FlorianWoelki@gmx.de>
2023-11-07 09:26:36 +0100

    refactor(plugin): adjust colorcolumn color

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -5,6 +5,8 @@ function ColorMe(color)
 
 	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+
+  vim.api.nvim_command("hi ColorColumn guibg=#32363d")
 end
 
 ColorMe()
