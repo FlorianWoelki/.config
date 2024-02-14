d051d02eecf15cf3d56a14c3e4e1c342e16b9923
FlorianWoelki <FlorianWoelki@gmx.de>
2024-02-14 11:14:05 +0100

    feat: add treesitter-context

--- lua/florianwoelki/packer.lua
+++ lua/florianwoelki/packer.lua
@@ -27,6 +27,8 @@ return require("packer").startup(function(use)
 	})
 
 	use("nvim-treesitter/nvim-treesitter", { run = ":TSUpdate" })
+	use("nvim-treesitter/nvim-treesitter-context")
+
 	use("tpope/vim-fugitive")
 
 	use({
