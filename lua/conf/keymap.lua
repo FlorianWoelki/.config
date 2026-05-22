9e0d47df535eb3986dd010806099bf65d7e1e4e7
FlorianWoelki <FlorianWoelki@gmx.de>
2026-05-22 08:49:37 +0200

    refactor(nvim): cleanup keymap file

--- lua/conf/keymap.lua
+++ lua/conf/keymap.lua
@@ -1,56 +1,58 @@
-local opts = { noremap = true, silent = true }
-local term_opts = { silent = true }
-local keymap = vim.api.nvim_set_keymap
-
-keymap("", "<Space>", "<Nop>", opts)
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
-keymap("i", "jk", "<Esc>", opts)
-keymap("v", "J", ":m '>+1<CR>gv=gv", opts)
-keymap("v", "K", ":m '<-2<CR>gv=gv", opts)
+local map = vim.keymap.set
+local opts = { noremap = true, silent = true }
+local term_opts = { silent = true }
 
--- Window navigation+resizing
--- keymap("n", "sh", "<C-w>h", opts)
--- keymap("n", "sk", "<C-w>k", opts)
--- keymap("n", "sj", "<C-w>j", opts)
--- keymap("n", "sl", "<C-w>l", opts)
-keymap("n", "ss", ":split<Return><C-w>j", opts)
-keymap("n", "sv", ":vsplit<Return><C-w>l", opts)
-keymap("n", "<leader>sl", ":vertical resize +10<Return>", opts)
-keymap("n", "<leader>sh", ":vertical resize -10<Return>", opts)
-keymap("n", "<leader>sj", ":horizontal resize +10<Return>", opts)
-keymap("n", "<leader>sk", ":horizontal resize -10<Return>", opts)
+-- Disable Space in normal and visual modes to prevent accidental movement 
+map({ "n", "v" }, "<Space>", "<Nop>", opts)
+
+-- Better escape
+map("i", "jk", "<Esc>", opts)
+
+-- Move highlighted text up and down in visual mode
+map("v", "J", ":m '>+1<CR>gv=gv", opts)
+map("v", "K", ":m '<-2<CR>gv=gv", opts)
+
+-- Window navigation and splitting
+map("n", "ss", ":split<Return><C-w>j", opts)
+map("n", "sv", ":vsplit<Return><C-w>l", opts)
+-- Window resizing
+map("n", "<leader>sl", ":vertical resize +10<Return>", opts)
+map("n", "<leader>sh", ":vertical resize -10<Return>", opts)
+map("n", "<leader>sj", ":horizontal resize +10<Return>", opts)
+map("n", "<leader>sk", ":horizontal resize -10<Return>", opts)
 
 -- Navigate buffers
-keymap("n", "<S-l>", ":bnext<CR>", opts)
-keymap("n", "<S-h>", ":bprevious<CR>", opts)
+map("n", "<S-l>", ":bnext<CR>", opts)
+map("n", "<S-h>", ":bprevious<CR>", opts)
+map("n", "gb", "<C-^>", opts)
 
-keymap("n", "gb", "<C-^>", opts)
+-- Better search navigation
+map("n", "n", "nzzzv", opts)
+map("n", "N", "Nzzzv", opts)
 
--- Better in file findings
-keymap("n", "n", "nzzzv", opts)
-keymap("n", "N", "Nzzzv", opts)
+-- Terminal window navigation
+map("t", "<C-h>", "<C-\\><C-N><C-w>h", term_opts)
+map("t", "<C-j>", "<C-\\><C-N><C-w>j", term_opts)
+map("t", "<C-k>", "<C-\\><C-N><C-w>k", term_opts)
+map("t", "<C-l>", "<C-\\><C-N><C-w>l", term_opts)
 
--- Terminal
-keymap("t", "<C-h>", "<C-\\><C-N><C-w>h", term_opts)
-keymap("t", "<C-j>", "<C-\\><C-N><C-w>j", term_opts)
-keymap("t", "<C-k>", "<C-\\><C-N><C-w>k", term_opts)
-keymap("t", "<C-l>", "<C-\\><C-N><C-w>l", term_opts)
+-- Half-page jumps up and down
+map("n", "<C-d>", "<C-d>zz", opts)
+map("n", "<C-u>", "<C-u>zz", opts)
 
--- Don't know what these do lol
-keymap("n", "<C-d>", "<C-d>zz", opts)
-keymap("n", "<C-u>", "<C-u>zz", opts)
+-- Disable Ex mode
+map("n", "Q", "<nop>", opts)
 
-keymap("n", "Q", "<nop>", opts)
-
-keymap("n", "<leader>y", '"+y', opts)
-keymap("v", "<leader>y", '"+y', opts)
+-- Copy to system clipboard
+map({ "n", "v" }, "<leader>y", '"+y', opts)
 
 -- New terminal in a horizontal / vertical / current window
-keymap("n", "<leader>th", ":20split | terminal<CR><cmd>startinsert<CR>", opts)
-keymap("n", "<leader>tv", ":80vsplit | terminal<CR><cmd>startinsert<CR>", opts)
-keymap("n", "<leader>tt", ":terminal<CR><cmd>startinsert<CR>", opts)
+map("n", "<leader>th", ":20split | terminal<CR><cmd>startinsert<CR>", opts)
+map("n", "<leader>tv", ":80vsplit | terminal<CR><cmd>startinsert<CR>", opts)
+map("n", "<leader>tt", ":terminal<CR><cmd>startinsert<CR>", opts)
 
 -- Custom command to replace :bd and keep windows open
 vim.api.nvim_create_user_command("Bd", function()
@@ -67,12 +69,11 @@ vim.api.nvim_create_user_command("Bd", function()
 
   -- Delete the original buffer
   vim.cmd("bdelete " .. bufnr)
-end, {})
+end, { desc = "Delete buffer but keep window open" })
 
 -- Abbreviate lowercase :bd to call our uppercase :Bd command
 vim.cmd("cnoreabbrev <expr> bd getcmdtype() == ':' && getcmdline() == 'bd' ? 'Bd' : 'bd'")
 
 -- Git
-keymap("n", "ghd", ":Gitsigns preview_hunk<CR>", opts)
-keymap("n", "ghr", ":Gitsigns reset_hunk<CR>", opts)
-keymap("v", "ghr", ":Gitsigns reset_hunk<CR>", opts)
+map("n", "ghd", ":Gitsigns preview_hunk<CR>", opts)
+map({ "n", "v" }, "ghr", ":Gitsigns reset_hunk<CR>", opts)
--- lua/conf/todo.md
+++ lua/conf/todo.md
@@ -1,5 +1,5 @@
 ## What still needs to be changed
 
 - [ ] Autocompletion should auto highlight without needing to press tab
-- [ ] Cleanup keymaps and files (don't use plugin names, just use general files like `ui.lua` and have multiple plugins in it)
+- [x] Cleanup keymaps and files (don't use plugin names, just use general files like `ui.lua` and have multiple plugins in it)
 
