05987223c838cde4a964bd1236ac235380b98618
FlorianWoelki <FlorianWoelki@gmx.de>
2026-05-27 08:21:43 +0200

    fix(nvim): remove OpenProjectTree function

--- lua/conf/plugins/telescope.lua
+++ lua/conf/plugins/telescope.lua
@@ -21,36 +21,11 @@ return {
     telescope.setup(opts)
 
     local builtin = require("telescope.builtin")
-    local actions = require("telescope.actions")
-    local action_state = require("telescope.actions.state")
-
-    local function open_project_and_tree()
-      telescope.extensions.projects.projects({
-        attach_mappings = function(prompt_bufnr, _)
-          actions.select_default:replace(function()
-            local selection = action_state.get_selected_entry()
-            local path = selection.path or selection.value
-
-            actions.close(prompt_bufnr)
-            vim.api.nvim_set_current_dir(path)
-
-            --vim.schedule(function()
-              --vim.cmd("Neotree show")
-            --end)
-          end)
-
-          return true
-        end,
-      })
-    end
-
-    _G.OpenProjectAndTree = open_project_and_tree
 
     vim.keymap.set("n", "<leader>pg", builtin.git_files, {})
     vim.keymap.set("n", "<leader>pr", builtin.live_grep, {})
     vim.keymap.set("n", "<leader>pf", builtin.find_files, {})
     vim.keymap.set("n", "<leader>pb", builtin.buffers, {})
     vim.keymap.set("n", "<leader>fh", ":Telescope find_files hidden=true <CR>")
-    vim.keymap.set("n", "<leader>pp", open_project_and_tree, { desc = "Projects" })
   end,
 }
